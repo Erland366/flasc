db7c4b15d6e7de11d6e0d6058d73ae715b102394
Erland366 <erland.pg366@gmail.com>
2025-02-21 20:41:55 +0400

    Refactor code structure for improved readability

--- train_sparse_lora_vanilla.py
+++ train_sparse_lora_vanilla.py
@@ -48,184 +48,194 @@ def parse():
     parser.add_argument("--entity", default=None, type=str)
     return parser.parse_args()
 
-args = parse()
-# os.environ['OMP_NUM_THREADS'] = '1'
-# os.environ['ROCR_VISIBLE_DEVICES'] = args.gpu
-# os.environ['HIP_VISIBLE_DEVICES'] = args.gpu
-if args.gpu != '0':
-    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+def main():
+    args = parse()
+    # os.environ['OMP_NUM_THREADS'] = '1'
+    # os.environ['ROCR_VISIBLE_DEVICES'] = args.gpu
+    # os.environ['HIP_VISIBLE_DEVICES'] = args.gpu
+    if args.gpu != '0':
+        os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
 
-import random
-import numpy as np
-import torch
-import tensorflow as tf
-tf.config.set_visible_devices([], device_type='GPU')
-print(f"Visible GPUs: {torch.cuda.device_count()}")
+    import random
+    import numpy as np
+    import torch
+    import tensorflow as tf
+    tf.config.set_visible_devices([], device_type='GPU')
+    print(f"Visible GPUs: {torch.cuda.device_count()}")
 
-random.seed(args.seed)
-np.random.seed(args.seed)
-torch.manual_seed(args.seed)
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
 
-def get_topk_mask(x, density):
-    mask = torch.zeros_like(x).bool()
-    k = int(x.numel()*density)
-    _, keep_idx = torch.topk(x, k=k)
-    mask[keep_idx] = 1
-    return mask
+    def get_topk_mask(x, density):
+        mask = torch.zeros_like(x).bool()
+        k = int(x.numel()*density)
+        _, keep_idx = torch.topk(x, k=k)
+        mask[keep_idx] = 1
+        return mask
 
-from train_utils import log_stats, login_wandb, is_wandb_available
+    from train_utils import log_stats, login_wandb, is_wandb_available
 
 
-def fl_train(save, run_dir, server_model, clients, valloader, testloader, test_batch,
-             rounds, eval_freq, eval_first, eval_masked, server_opt,
-             server_batch, server_lr, server_freeze, client_lr, client_epochs, client_freeze, 
-             l2_clip_norm=0, noise_multiplier=0):
-    if args.use_tensorboard:
-        writer = tf.summary.create_file_writer(run_dir)
-    else:
-        writer = None
+    def fl_train(save, run_dir, server_model, clients, valloader, testloader, test_batch,
+                rounds, eval_freq, eval_first, eval_masked, server_opt,
+                server_batch, server_lr, server_freeze, client_lr, client_epochs, client_freeze, 
+                l2_clip_norm=0, noise_multiplier=0):
+        if args.use_tensorboard:
+            writer = tf.summary.create_file_writer(run_dir)
+        else:
+            writer = None
 
-    if args.use_wandb:
-        if not is_wandb_available():
-            raise ValueError("Wandb is not available")
+        if args.use_wandb:
+            if not is_wandb_available():
+                raise ValueError("Wandb is not available")
+            
+            import wandb
+
+            login_wandb()
+            wandb.init(
+                project=args.project_name,
+                entity=args.entity,
+                name=args.wandb_name,
+                config=vars(args),
+            )
+
+        pbar = tqdm(range(rounds))
+
+        server_params = {n:p for n,p in server_model.named_parameters() if p.requires_grad}
+        server_mask = {n:torch.ones_like(p) for n,p in server_params.items()}
+        if server_opt == 'sgd':
+            server_opt = torch.optim.SGD(server_params.values(), lr=server_lr)
+        elif server_opt == 'adam':
+            server_opt = torch.optim.AdamW(server_params.values(), lr=server_lr)
+        else:
+            raise ValueError()
+        sched = torch.optim.lr_scheduler.StepLR(server_opt, step_size=1, gamma=1)
+
+        eval_accu = 0
+        def eval_loop(model, loader):        
+            model.eval()
+            stats_acc = {}
+            for x,y in loader:
+                with torch.no_grad():
+                    _, stats = test_batch(model, x, y)
+                for k,v in stats.items():
+                    stats_acc[k] = stats_acc.get(k, 0) + v
+            stats_acc['loss'] /= stats_acc['count']
+            return stats_acc
         
+        if eval_first:
+            # I think this is still error?
+            log_stats(writer, "eval", stats, 0)
+        
+        for rnd in pbar:
+            aggregate = None
+            stats_acc = {}
+            client_ids = torch.randperm(len(clients))[:server_batch]
+            for i,client_id in enumerate(client_ids):
+                # Download Sparsity
+                client_model = deepcopy(server_model)
+
+                # Local Training
+                client_opt = torch.optim.SGD(client_model.parameters(), lr=client_lr, momentum=0.9)
+                client_loader = clients[client_id]
+                client_acc = {}
+                for epoch in range(client_epochs):
+                    for x,y in client_loader:
+                        loss, stats = test_batch(client_model, x, y)
+                        client_opt.zero_grad()
+                        loss.backward()
+
+                        client_opt.step()
+                        for k,v in stats.items():
+                            client_acc[k] = client_acc.get(k, 0) + v
+                        pbar.set_description(f"eval: {eval_accu} | client {i}, epoch {epoch} | loss {loss:.4f}")
+
+                neg_client_delta = {n: server_params[n].data - cp.data for n,cp 
+                                    in client_model.named_parameters() if cp.requires_grad}
+
+                # Aggregation
+                if aggregate is None:
+                    aggregate = neg_client_delta
+                else:
+                    for n, delta in neg_client_delta.items():
+                        aggregate[n] += delta
+                # Log last iteration
+                client_acc['norm'] = 0
+                for k,v in client_acc.items():
+                    stats_acc[k] = stats_acc.get(k, 0) + v
+
+            # Server model update
+            server_opt.zero_grad()
+            for n, sp in server_params.items():
+                sp.grad = aggregate[n] / server_batch
+            server_opt.step()
+            sched.step()
+            # Eval and Logging
+            if (rnd+1) % eval_freq == 0:
+                eval_model = deepcopy(server_model)
+                if valloader is not None:
+                    log_stats(writer, "eval", eval_loop(eval_model, valloader), rnd+1)
+                log_stats(writer, "test", eval_loop(eval_model, testloader), rnd+1)
+            
+            stats_acc['norm'] /= server_batch
+            stats_acc['loss'] /= stats_acc['count']
+            log_stats(writer, "train", stats_acc, rnd+1)
+
+            pbar.set_description(f"eval: {eval_accu}")
+        # if save:
+        #     torch.save({'delta': server_params, 'mask': server_mask}, f"{run_dir}/save.pt")
+
+    import data_utils
+    clients, valloader, testloader, test_batch = data_utils.build_dataset(
+        args.dataset, args.client_batch, args.clients, args.iid_alpha, args.seed, args.eval_frac)
+
+    import models
+    model = models.build_model(args.dataset)
+    total = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    models.add_adapters_dataset(args.dataset, model, args.lora_rank, args.lora_alpha)
+    if str2bool(args.freeze_a):
+        for n,p in model.named_parameters():
+            if "lora_A" in n:
+                p.requires_grad = False
+    trainable = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(f"Training {trainable} parameters ({100*trainable/total:.2f}% of original {total})")
+    model = model.cuda()
+
+    # Add hash4 at the end?
+    args.name += f"_{args.model}_c{args.clients}_b{args.client_batch}_lr{args.client_lr}_e{args.client_epochs}"
+    args.wandb_name = f"{args.name}"
+    # add name with date
+    args.run_dir_name = f"{args.dir}/{args.name}_{time.strftime('%Y%m%d-%H%M%S')}"
+
+    run_dir = args.run_dir_name
+    os.makedirs(run_dir)
+    import json
+    with open(f"{run_dir}/args.json", 'w') as f:
+    json.dump(vars(args), f, indent=4)
+    print(f"Saved args to {run_dir}")
+
+    fl_train(str2bool(args.save),
+        run_dir, model, clients, valloader, testloader, test_batch,
+        rounds=args.server_rounds, 
+        eval_freq=args.eval_freq,
+        eval_first=str2bool(args.eval_first),
+        eval_masked=str2bool(args.eval_masked),
+        server_opt=args.server_opt,
+        server_batch=args.server_batch, 
+        server_lr=args.server_lr,
+        server_freeze=str2bool(args.server_freeze),
+        client_lr=args.client_lr, 
+        client_epochs=args.client_epochs,
+        client_freeze=str2bool(args.client_freeze),
+        l2_clip_norm=args.l2_clip_norm,
+        noise_multiplier=args.noise_multiplier)
+
+    if is_wandb_available():
         import wandb
 
-        login_wandb()
-        wandb.init(
-            project=args.project_name,
-            entity=args.entity,
-            name=args.wandb_name,
-            config=vars(args),
-        )
+        if wandb.run is not None:
+            wandb.finish()  
 
-    pbar = tqdm(range(rounds))
-
-    server_params = {n:p for n,p in server_model.named_parameters() if p.requires_grad}
-    server_mask = {n:torch.ones_like(p) for n,p in server_params.items()}
-    if server_opt == 'sgd':
-        server_opt = torch.optim.SGD(server_params.values(), lr=server_lr)
-    elif server_opt == 'adam':
-        server_opt = torch.optim.AdamW(server_params.values(), lr=server_lr)
-    else:
-        raise ValueError()
-    sched = torch.optim.lr_scheduler.StepLR(server_opt, step_size=1, gamma=1)
-
-    eval_accu = 0
-    def eval_loop(model, loader):        
-        model.eval()
-        stats_acc = {}
-        for x,y in loader:
-            with torch.no_grad():
-                _, stats = test_batch(model, x, y)
-            for k,v in stats.items():
-                stats_acc[k] = stats_acc.get(k, 0) + v
-        stats_acc['loss'] /= stats_acc['count']
-        return stats_acc
-    
-    if eval_first:
-        # I think this is still error?
-        log_stats(writer, "eval", stats, 0)
-    
-    for rnd in pbar:
-        aggregate = None
-        stats_acc = {}
-        client_ids = torch.randperm(len(clients))[:server_batch]
-        for i,client_id in enumerate(client_ids):
-            # Download Sparsity
-            client_model = deepcopy(server_model)
-
-            # Local Training
-            client_opt = torch.optim.SGD(client_model.parameters(), lr=client_lr, momentum=0.9)
-            client_loader = clients[client_id]
-            client_acc = {}
-            for epoch in range(client_epochs):
-                for x,y in client_loader:
-                    loss, stats = test_batch(client_model, x, y)
-                    client_opt.zero_grad()
-                    loss.backward()
-
-                    client_opt.step()
-                    for k,v in stats.items():
-                        client_acc[k] = client_acc.get(k, 0) + v
-                    pbar.set_description(f"eval: {eval_accu} | client {i}, epoch {epoch} | loss {loss:.4f}")
-
-            neg_client_delta = {n: server_params[n].data - cp.data for n,cp 
-                                in client_model.named_parameters() if cp.requires_grad}
-
-            # Aggregation
-            if aggregate is None:
-                aggregate = neg_client_delta
-            else:
-                for n, delta in neg_client_delta.items():
-                    aggregate[n] += delta
-            # Log last iteration
-            client_acc['norm'] = 0
-            for k,v in client_acc.items():
-                stats_acc[k] = stats_acc.get(k, 0) + v
-
-        # Server model update
-        server_opt.zero_grad()
-        for n, sp in server_params.items():
-            sp.grad = aggregate[n] / server_batch
-        server_opt.step()
-        sched.step()
-        # Eval and Logging
-        if (rnd+1) % eval_freq == 0:
-            eval_model = deepcopy(server_model)
-            if valloader is not None:
-                log_stats(writer, "eval", eval_loop(eval_model, valloader), rnd+1)
-            log_stats(writer, "test", eval_loop(eval_model, testloader), rnd+1)
-        
-        stats_acc['norm'] /= server_batch
-        stats_acc['loss'] /= stats_acc['count']
-        log_stats(writer, "train", stats_acc, rnd+1)
-
-        pbar.set_description(f"eval: {eval_accu}")
-    # if save:
-    #     torch.save({'delta': server_params, 'mask': server_mask}, f"{run_dir}/save.pt")
-
-import data_utils
-clients, valloader, testloader, test_batch = data_utils.build_dataset(
-    args.dataset, args.client_batch, args.clients, args.iid_alpha, args.seed, args.eval_frac)
-
-import models
-model = models.build_model(args.dataset)
-total = sum(p.numel() for p in model.parameters() if p.requires_grad)
-models.add_adapters_dataset(args.dataset, model, args.lora_rank, args.lora_alpha)
-if str2bool(args.freeze_a):
-    for n,p in model.named_parameters():
-        if "lora_A" in n:
-            p.requires_grad = False
-trainable = sum(p.numel() for p in model.parameters() if p.requires_grad)
-print(f"Training {trainable} parameters ({100*trainable/total:.2f}% of original {total})")
-model = model.cuda()
-
-# Add hash4 at the end?
-args.name += f"_{args.model}_c{args.clients}_b{args.client_batch}_lr{args.client_lr}_e{args.client_epochs}"
-args.wandb_name = f"{args.name}"
-# add name with date
-args.run_dir_name = f"{args.dir}/{args.name}_{time.strftime('%Y%m%d-%H%M%S')}"
-
-run_dir = args.run_dir_name
-os.makedirs(run_dir)
-import json
-with open(f"{run_dir}/args.json", 'w') as f:
-  json.dump(vars(args), f, indent=4)
-  print(f"Saved args to {run_dir}")
-
-fl_train(str2bool(args.save),
-    run_dir, model, clients, valloader, testloader, test_batch,
-    rounds=args.server_rounds, 
-    eval_freq=args.eval_freq,
-    eval_first=str2bool(args.eval_first),
-    eval_masked=str2bool(args.eval_masked),
-    server_opt=args.server_opt,
-    server_batch=args.server_batch, 
-    server_lr=args.server_lr,
-    server_freeze=str2bool(args.server_freeze),
-    client_lr=args.client_lr, 
-    client_epochs=args.client_epochs,
-    client_freeze=str2bool(args.client_freeze),
-    l2_clip_norm=args.l2_clip_norm,
-    noise_multiplier=args.noise_multiplier)
+if __name__ == "__main__":
+    main()
