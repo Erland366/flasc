996083ba151603a2e291e017b8b8ed73835b0ff8
Erland366 <erland.pg366@gmail.com>
2025-02-28 01:31:25 +0400

    Add feddare

--- merging.py
+++ merging.py
@@ -50,11 +50,39 @@ class FedProx(BaseMerging):
             
         return aggregate
 
+class FedDare(BaseMerging):
+    def __init__(self, server_model: nn.Module, p_drop: float = 0.5):
+        super().__init__(server_model)
+        self.p_drop = p_drop
+
+    def aggregate_updates(self, client_deltas: List[Dict[str, torch.Tensor]]) -> Dict[str, torch.Tensor]:
+        # We dropped several params and rescale the rest just like in Dropout
+        batch_size = len(client_deltas)
+
+        aggregate = {}
+
+        for n, p in client_deltas[0].items():
+            aggregate[n] = p.clone()
+
+        # randomly drop some params based on bernoulli distribution
+        for i in range(1, batch_size):
+            for n, p in client_deltas[i].items():
+                if torch.rand(1) < self.p:
+                    aggregate[n] += p / (1 - self.p)
+
+        # Then average
+        for n in aggregate:
+            aggregate[n] /= batch_size
+
+        return aggregate
+                
+
 
 class MergingFactory:
     STRATEGY_PARAMS = {
         "fedavg": [],
-        "fedprox" : ["mu"]
+        "fedprox" : ["mu"],
+        "feddare" : ["p_drop"]
     }
     @staticmethod
     def get_merging_strategy(strategy: str, server_model: nn.Module, args=None, **kwargs) -> BaseMerging:
@@ -91,7 +119,11 @@ class MergingFactory:
         if strategy == "fedavg":
             return FedAvg(server_model)
         elif strategy == "fedprox":
+            # TODO: Double check when we have the args + kwargs (pass the params through argparse)
             mu = merging_kwargs.get("mu", 0.01)
             return FedProx(server_model, mu=mu)
+        elif strategy == "feddare":
+            p_drop = merging_kwargs.get("p_drop", 0.5)
+            return FedDare(server_model, p_drop=p_drop)
         else:
             raise ValueError(f"Unknown merging strategy: {strategy}")
